d3eb42aeaf5ce2d288dc9205cf20718abccc5379
qzz0518 <16370772+qzz0518@users.noreply.github.com>
2026-09-04 06:22:23 +0900

    Update AutoCodeBar cask to 2.0.1

--- Casks/autocodebar.rb
+++ Casks/autocodebar.rb
@@ -1,6 +1,6 @@
 cask "autocodebar" do
-  version "2.0.0"
-  sha256 "5d7abe0b5ab491fd231105e5dc1a4812db69d50df870367a0393dc224b80fc3d"
+  version "2.0.1"
+  sha256 "ed2cf66c8496248ade6f036cfe20d4048ba007d57d3c4d57c3837223d9c74229"
 
   url "https://github.com/qzz0518/AutoCodeBar/releases/download/v#{version}/AutoCodeBar-#{version}.dmg"
   name "AutoCodeBar"
